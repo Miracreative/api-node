cd5a47a427531e93269e002832876236eb1103e8
Nana <nanalit16@gmail.com>
2024-10-23 15:58:09 +0300

    atmans

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
 const  Pool  = require("pg").Pool;
 
 const pool = new Pool({
-    user: 'nanalit',
+    user: 'atman',
     password: 's123s345',
     host: '127.0.0.1',
     port: 5432,
     database: "atman"
 })
-
-console.log(pool.log)
 
 module.exports = pool
