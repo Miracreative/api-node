a9dd1a1429a076ce7f42b0b4c0a9347dced0a690
Nana <nanalit16@gmail.com>
2024-10-28 11:10:48 +0300

    feat: return db.js

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
-// const  Pool  = require("pg").Pool;
-
-// const pool = new Pool({
-//     user: 'postgres',
-//     password: 's123s345',
-//     host: 'localhost',
-//     port: 5432,
-//     database: "node_postgres"
-// })
-
-// module.exports = pool
-
 const  Pool  = require("pg").Pool;
 
 const pool = new Pool({
-    user: 'atman',
+    user: 'postgres',
     password: 's123s345',
     host: 'localhost',
     port: 5432,
-    database: "atman"
+    database: "node_postgres"
 })
 
 module.exports = pool
+
+// const  Pool  = require("pg").Pool;
+
+// const pool = new Pool({
+//     user: 'atman',
+//     password: 's123s345',
+//     host: 'localhost',
+//     port: 5432,
+//     database: "atman"
+// })
+
+// module.exports = pool
