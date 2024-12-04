0cd529d834ab726e61c739b05669912111c121e4
Nana <nanalit16@gmail.com>
2024-12-04 11:12:23 +0300

    afoam

--- controller/afoam.controler.js
+++ controller/afoam.controler.js
@@ -3,12 +3,30 @@ const db = require('../db');
 // const fs = require('fs');
 
 class AfoamController {
+    // async getAllAfoam(req, res) {
+    //     try {
+    //         const recomendedGoods = await db.query(
+    //             `SELECT id, advantages, name, description FROM goods ORDER BY id DESC LIMIT 5`,
+    //         );
+    //         res.json(recomendedGoods.rows);
+    //     } catch (e) {
+    //         return res.status(404).json({ message: e.message });
+    //     }
+    // }
     async getAllAfoam(req, res) {
+        const string = 'AFOAM';
         try {
-            const recomendedGoods = await db.query(
-                `SELECT id, advantages, name, description FROM goods ORDER BY id DESC LIMIT 5`,
-            );
-            res.json(recomendedGoods.rows);
+            const news = await db.query(`SELECT * FROM goods`);
+
+            let result = [];
+            news.rows.forEach((item) => {
+                if (
+                    item.name.toLowerCase().includes(string.toLowerCase())
+                ) {
+                    result.push(item);
+                }
+            });
+            res.json(result);
         } catch (e) {
             return res.status(404).json({ message: e.message });
         }
