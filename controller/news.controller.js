9b23e9ec0865a9c22d814486bc5a1a9b7206d19e
Nana <nanalit16@gmail.com>
2025-02-05 11:53:14 +0300

    saaaf

--- controller/news.controller.js
+++ controller/news.controller.js
@@ -116,30 +116,30 @@ class NewsController {
         try {
 
             const carouselImages = req.files?.files;
+            console.log(req.files?.files)
+        //     if(carouselImages) {
+        //         let imagesSrc = [];
+        //             carouselImages.map((file, index) => {
+        //                 imagesSrc.push(`${file.filename}`);
+        //             });
+        //         await db.query(
+        //             `UPDATE news SET imagesSrc = $1 WHERE id = $2 RETURNING *`,
+        //             [imagesSrc, id])
+        //     }
 
-            if(carouselImages) {
-                let imagesSrc = [];
-                    carouselImages.map((file, index) => {
-                        imagesSrc.push(`${file.filename}`);
-                    });
-                await db.query(
-                    `UPDATE news SET imagesSrc = $1 WHERE id = $2 RETURNING *`,
-                    [imagesSrc, id])
-            }
-
-            const main = `${req.files?.mainimage[0]?.filename}`;
-            if(main) {
-                await db.query(
-                    `UPDATE news SET main = $1 WHERE id = $2 RETURNING *`,
-                    [main, id])
-            }
-            // Выполняем обновление в базе данных
-            const updatedNews = await db.query(
-                `UPDATE news SET title = $2, descr = $3, content = $4 WHERE id = $6 RETURNING *`,
-                [title, descr, content, id]
-            );
+        //     const main = `${req.files?.mainimage[0]?.filename}`;
+        //     if(main) {
+        //         await db.query(
+        //             `UPDATE news SET main = $1 WHERE id = $2 RETURNING *`,
+        //             [main, id])
+        //     }
+        //     // Выполняем обновление в базе данных
+        //     const updatedNews = await db.query(
+        //         `UPDATE news SET title = $2, descr = $3, content = $4 WHERE id = $6 RETURNING *`,
+        //         [title, descr, content, id]
+        //     );
     
-           return  res.json(updatedNews.rows);
+        //    return  res.json(updatedNews.rows);
         } catch (e) {
             console.error('Ошибка при обновлении новости:', e.message);
             return res.status(500).json({ message: 'Ошибка при обновлении новости', error: e.message });
