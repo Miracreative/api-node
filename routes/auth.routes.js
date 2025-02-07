7684bacbcb62b6a6f4604d3244b960520b55974d
Nana <nanalit16@gmail.com>
2025-02-07 10:59:45 +0300

    passport

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,7 +5,9 @@ const authController = require('../controller/auth.controller');
 
 const authRouter = new Router();
 
-authRouter.post('/login', authController.login)
+authRouter.post('/login',
+    [passport.authenticate('jwt', {session: false}), ], 
+    authController.login)
 authRouter.post('/logout', authController.logout)
 authRouter.get('/refresh/:refresh_token', authController.refresh)
 authRouter.post('/registration' , 
