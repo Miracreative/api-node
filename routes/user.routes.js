04a1c48265b323cfe3c29c484630217fe74479b4
Nana <nanalit16@gmail.com>
2025-02-07 11:33:31 +0300

    weaaqa

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,7 +6,7 @@ const passport = require('passport')
 
 userRouter.get('/users',  userController.getUsers)
 userRouter.get('/users/:id',
-    [passport.authenticate('jwt', {session: false}), ], 
+    [passport.authenticate('jwt', {session: true}), ], 
     userController.getOneUser) // через слеш
 userRouter.put('/users', userController.updateUser)
 userRouter.delete('/users/:id', userController.deleteUser)
