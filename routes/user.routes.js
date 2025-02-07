db677b900f308053108d8fd3ee034ff2361a239a
Nana <nanalit16@gmail.com>
2025-02-07 11:17:27 +0300

    sd

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,8 +4,10 @@ const userController = require('./../controller/user.controller')
 
 
 
-userRouter.get('/users', userController.getUsers)
-userRouter.get('/users/:id', userController.getOneUser) // через слеш
+userRouter.get('/users',  userController.getUsers)
+userRouter.get('/users/:id',
+    [passport.authenticate('jwt', {session: false}), ], 
+    userController.getOneUser) // через слеш
 userRouter.put('/users', userController.updateUser)
 userRouter.delete('/users/:id', userController.deleteUser)
 
