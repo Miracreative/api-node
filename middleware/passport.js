8f898366fa11d415675c8c3c5ba777e1b05f6144
Nana <nanalit16@gmail.com>
2025-02-07 11:32:15 +0300

    weaq

--- middleware/passport.js
+++ middleware/passport.js
@@ -10,12 +10,12 @@ module.exports = passport => {
     passport.use(new JwtStrategy(options, async function(payload, done) {
         const user = await db.query(`SELECT * FROM users WHERE id = $1::int`, [payload.id])
         try {
-            if(user.rows[0].role == 'admin') {
-                console.log('admin')
-            } else {
-                console.log('user')
-                done(null, false)
-            }
+            // if(user.rows[0].role == 'admin') {
+            //     console.log('admin')
+            // } else {
+            //     console.log('user')
+            //     done(null, false)
+            // }
             done(null, user)
         } catch(e) {
             console.log(e)
