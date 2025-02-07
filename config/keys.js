6f42ae0b83da1f65ee025118c2e926ac5ff2ef2e
Nana <nanalit16@gmail.com>
2025-02-07 17:14:52 +0300

    front url

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     base_url: 'http://localhost:5000/api',
     del_url: './uploads/',
-    front_url: 'https://admin-panel.atman-auto.ru/',
+    front_url: 'https://admin-panel.atman-auto.ru',
     jwt: 'dev-jwt',
     refresh: 'dev-refresh'
 }
