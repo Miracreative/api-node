f35e794f557e0264e94c7d20359c0e677e5b59cb
Nana <nanalit16@gmail.com>
2025-02-12 14:22:39 +0300

    passport sertificate

--- routes/sertificate.routes.js
+++ routes/sertificate.routes.js
@@ -1,16 +1,16 @@
 const Router = require('express')
 const sertificateController = require('../controller/sertificate.controller');
 const upload = require('../middleware/upload')
+const passport = require('passport')
+const sertificateRouter = new Router(); 
 
-const sertificateRouter = new Router();
-
-sertificateRouter.post('/sertificate', upload.single('file'), sertificateController.createSertificate)
+sertificateRouter.post('/sertificate',  [passport.authenticate('jwt', {session: false}), upload.single('file')], sertificateController.createSertificate)
 sertificateRouter.get('/sertificate/:page', sertificateController.getPaginationSertificates)
 sertificateRouter.get('/sertificate', sertificateController.getAllSertificates)
 sertificateRouter.get('/sertificate-one/:id', sertificateController.getOneSertificate) 
 sertificateRouter.get('/sertificate-search/:string', sertificateController.getSearchSertificates)
-sertificateRouter.put('/sertificate', upload.single('file'), sertificateController.updateSertificate)
-sertificateRouter.delete('/sertificate/:id', sertificateController.deleteSertificate)
+sertificateRouter.put('/sertificate',  [passport.authenticate('jwt', {session: false}), upload.single('file')], sertificateController.updateSertificate)
+sertificateRouter.delete('/sertificate/:id',  [passport.authenticate('jwt', {session: false})], sertificateController.deleteSertificate)
 
 
 module.exports = sertificateRouter;  
