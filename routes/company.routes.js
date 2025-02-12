1e424d9585294001d491f61270a4020d61e5b4ce
Nana <nanalit16@gmail.com>
2025-02-12 13:31:23 +0300

    passport company

--- routes/company.routes.js
+++ routes/company.routes.js
@@ -1,11 +1,17 @@
+const passport = require('passport')
 const Router = require('express')
 const companyController = require('../controller/company.controller');
 const upload = require('../middleware/upload')
 const companyRouter = new Router();
 
-companyRouter.post('/company', upload.single('file'), companyController.createCompany)
+companyRouter.post('/company', 
+    [passport.authenticate('jwt', {session: false}), upload.single('file')],
+    companyController.createCompany)
 companyRouter.get('/company',  companyController.getAllCompany)
-companyRouter.put('/company', upload.single('file'), companyController.updateCompany)
-companyRouter.delete('/company/:id',  companyController.deleteCompany)
+companyRouter.put('/company', 
+    [passport.authenticate('jwt', {session: false}), upload.single('file')], companyController.updateCompany)
+companyRouter.delete('/company/:id', 
+    [passport.authenticate('jwt', {session: false})],
+    companyController.deleteCompany)
 
-module.exports = companyRouter;  
+module.exports = companyRouter;   
